0c798406f68c3aab6c3f5735419c5d4e88c699cf
Nethul <nethul2011@gmail.com>
2025-07-13 21:57:04 -0700

    Bug fixes

--- js/auth.js
+++ js/auth.js
@@ -1,5 +1,11 @@
+// --- Create the Supabase Client ---
+const { createClient } = supabase; 
+const SUPABASE_URL = 'https://ttocgvyuaktyxzubajjq.supabase.co'; 
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR0b2Nndnl1YWt0eXh6dWJhampxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI0NjQ1MjIsImV4cCI6MjA2ODA0MDUyMn0.mkzqkHj2Lb4SwxwqbZ3YbesxPa0dIPt8gOvfdhHEwqM';
 
-import { supabase } from './supabaseClient.js';
+
+const supabase_client = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
 
 class AuthManager {
 
--- js/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-
-import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = 'https://ttocgvyuaktyxzubajjq.supabase.co'; 
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR0b2Nndnl1YWt0eXh6dWJhampxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI0NjQ1MjIsImV4cCI6MjA2ODA0MDUyMn0.mkzqkHj2Lb4SwxwqbZ3YbesxPa0dIPt8gOvfdhHEwqM';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
